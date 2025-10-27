3dc78d09d20634b2dc00f41da8c19a6be5526cb1
HellenGabi <hellen_scarantti@estudante.sesisenai.org.br>
2025-10-27 19:45:18 -0300

    terminando

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,10 +22,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AlunoDAO alunoDAO = new AlunoDAO();
-        ProfessorDAO professorDAO = new ProfessorDAO();
-        CursoDAO cursoDAO = new CursoDAO();
-        MatriculaDAO matriculaDAO = new MatriculaDAO();
 
         while (true) {
             // Exibir o menu
